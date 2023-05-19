89ce3b237d1c119650f985be71eb53d50fcd9648
kotikkir9 <kirillzhukau@outlook.com>
2023-05-19 17:59:00 +0200

    Revert the changes

--- app.py
+++ app.py
@@ -38,7 +38,7 @@ app = TelegramClient("my_account", os.getenv('API_ID'), os.getenv('API_HASH'))
 
 
 async def send(chat, message, save=True):
-    message = await app.send_message(str(chat), message)
+    message = await app.send_message(chat, message)
     if save:
         message_ids.append(message.id)
 
